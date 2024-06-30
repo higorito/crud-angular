70598203efc4c66b876e8f61b3cb4b3926dd16a5
higor <higorps198@gmail.com>
2024-06-30 16:56:25 -0300

    add interface p tipar, add table e iterando do TS pro html

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Frontend' },
+    { _id: '2', name: 'React', category: 'Frontend' },
+    { _id: '3', name: 'Spring Boot', category: 'Backend' }
+  ];
+
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    //   this.courses = [];
+  }
 
 }
