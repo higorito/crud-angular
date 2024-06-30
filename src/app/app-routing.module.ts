b5832c4493da9c7f3396de15384366b2e0b82e61
higor <higorps198@gmail.com>
2024-06-30 16:37:53 -0300

    criando modulo e depois componente, adicionando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'courses', pathMatch: 'full' //o full p verifica td
+  },
+  {
+    path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
