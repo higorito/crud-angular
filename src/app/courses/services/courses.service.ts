c515d7f039af1cdc03afdb09a23e1b30e421cf83
higor <higorps198@gmail.com>
2024-07-01 16:09:29 -0300

    criando um proxy conectar no back npm run start

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { delay, first, take, tap } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   //so de colocar no construtor o angular ja entende que é para injetar o serviço
   constructor(private httpClient: HttpClient) {
